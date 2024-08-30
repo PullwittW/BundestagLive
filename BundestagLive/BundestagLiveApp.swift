24a54b99b5d37331eaa5f68619ea9217ecea932f
Wangu Pullwitt <wangupullwitt.priv@gmail.com>
2024-08-30 20:56:38 +0200

    Initial Commit

---
+++ BundestagLive/BundestagLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundestagLiveApp.swift
+//  BundestagLive
+//
+//  Created by Wangu Pullwitt on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct BundestagLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
